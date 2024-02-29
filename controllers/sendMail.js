467d0ef043fc8ddcb3c47819c3d0d0071da59ab3
AbdulAmir1 <abdulamir0@gmail.com>
2024-03-01 01:25:10 +0300

    controllers/sendMail.js can send tes mail successfully

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -7,20 +7,81 @@ const create = async (req, res) => {
   }
 
   try {
-    res.send('create() method called.')
+    var transporter = nodemailer.createTransport({
+      host: 'smtp.gmail.com',
+      port: 465,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS
+      }
+    })
+
+    transporter.verify((error, success) => {
+      if (error) {
+        console.log(error)
+      } else {
+        console.log('All works fine, congratz!')
+      }
+    })
+
+    var mail = {
+      from: 'AAmir Saleh',
+      to: 'blank.line@outlook.com',
+      subject: 'Test Mail from Booking proj',
+      html: 'This is a test email message sent to you from aamiralmayad@gmail.com'
+    }
+
+    transporter.sendMail(mail, (err, data) => {
+      if (err) {
+        res.json({
+          msg: 'fail'
+        })
+      } else {
+        res.json({
+          msg: 'success'
+        })
+      }
+    })
+    // res.send('create() method called.')
   } catch (err) {
     res.send(`error calling create ${err}`)
   }
   /* 
-  var transport = nodemailer.createTransport({
+  var transporter = nodemailer.createTransport({
     host: "sandbox.smtp.mailtrap.io",
     port: 2525,
     auth: {
-      user: "bb03a9478dca66",
-      pass: "dbb6b2a1be929d"
+      user: process.env.MAIL_USER,
+      pass: process.env.MAIL_PASS
     }
   });
 
+  transporter.verify((error, success) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('All works fine, congratz!');
+  }
+});
+
+var mail = {
+    from: name,
+    to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',
+    subject: 'Contact form request',
+    html: message
+  }
+
+transporter.sendMail(mail, (err, data) => {
+    if (err) {
+      res.json({
+        msg: 'fail'
+      })
+    } else {
+      res.json({
+        msg: 'success'
+      })
+    }
+  })
  */
 }
 

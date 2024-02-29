ffb8da7a9b302e5683e626ba5d30c9e6be67f78d
Yasmeen <yomalbalooshi@gmail.com>
2024-02-29 13:10:18 +0300

    hotel,review,room schemas

---
+++ models/Review.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+const ReviewSchema = new Schema(
+  {
+    feedback: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 5,
+      default: 5
+    },
+    creationDate: function () {
+      return new Date().getFullYear()
+    },
+    customerId: { type: Schema.Types.ObjectId, ref: 'Customer' }
+  },
+  { timestamps: true }
+)
+module.exports = mongoose.model('Review', ReviewSchema)

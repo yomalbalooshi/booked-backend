78db61efdc2a9078b41f9c8ed08bbdb745ffcb84
AbdulAmir1 <abdulamir0@gmail.com>
2024-03-02 23:09:11 +0300

    controllers/sendMail.js and routes/sendMail.js are created.

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const create = async (req, res) => {
   // Remove empty properties so that defaults will be applied
-
+  require('dotenv').config()
   for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key]
   }
@@ -16,19 +16,21 @@ const create = async (req, res) => {
       }
     })
 
+    console.log('process.env.MAIL_USER', process.env.MAIL_USER)
+    console.log('process.env.MAIL_PASS', process.env.MAIL_PASS)
     transporter.verify((error, success) => {
       if (error) {
-        console.log(error)
+        console.log('error----------', error)
       } else {
         console.log('All works fine, congratz!')
       }
     })
 
     var mail = {
-      from: 'AAmir Saleh',
-      to: 'blank.line@outlook.com',
-      subject: 'Test Mail from Booking proj',
-      html: 'This is a test email message sent to you from aamiralmayad@gmail.com'
+      from: req.body.name,
+      to: req.body.email,
+      subject: 'Test Mail from nodemailer-test1 proj',
+      html: req.body.messageHtml
     }
 
     transporter.sendMail(mail, (err, data) => {
@@ -42,47 +44,11 @@ const create = async (req, res) => {
         })
       }
     })
-    // res.send('create() method called.')
+
+    // console.log('req.body :', req.body)
   } catch (err) {
     res.send(`error calling create ${err}`)
   }
-  /* 
-  var transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-      user: process.env.MAIL_USER,
-      pass: process.env.MAIL_PASS
-    }
-  });
-
-  transporter.verify((error, success) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log('All works fine, congratz!');
-  }
-});
-
-var mail = {
-    from: name,
-    to: 'RECEIVING_EMAIL_ADDRESS_GOES_HERE',
-    subject: 'Contact form request',
-    html: message
-  }
-
-transporter.sendMail(mail, (err, data) => {
-    if (err) {
-      res.json({
-        msg: 'fail'
-      })
-    } else {
-      res.json({
-        msg: 'success'
-      })
-    }
-  })
- */
 }
 
 module.exports = {
--- routes/sendMail.js
+++ routes/sendMail.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const router = express.Router()
 const sendMailCtrl = require('../controllers/sendMail')
-//create a hotel
 router.post('/send', sendMailCtrl.create)
 module.exports = router

0fe41418def02bfe39af203178a644dbc2aa8ee2
AbdulAmir1 <abdulamir0@gmail.com>
2024-03-04 09:01:52 +0300

    deleted unrequired code in routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,6 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-// CAN BE DELETED LATER
-router.get('/example/:id/subs/:sub1/:sub2/data', function (req, res, next) {
-  console.log(req.params)
-  res.send('Testing 2')
-})
-
 //create a hotel review
 // router.post('/hotels/:id/reviews', hotelsCtrl.createReview)
 module.exports = router
